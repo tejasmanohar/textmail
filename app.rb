38bbdd61fe7970985d67153c3835f8d51aa07020
Tejas Manohar <me@tejasmanohar.com>
2014-11-09 09:12:14 +0000

    done

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-require 'gmail-ruby'
+require 'gmail'
 require 'twilio-ruby'
 require 'bundler'
 
@@ -10,10 +10,16 @@ configure :development do
   set :port, 3000
 end
 
+Twilio.configure do |config|
+  config.account_sid = 'AC881f1d62823ef5127e2b2a7a1afa30f1'
+  config.auth_token = 'd743a50ba1688eba1cdba47a4eb63e51'
+end
+
 get '/' do  
 end
 
 get '/receive' do
+  @client = Twilio::REST::Client.new
   puts params[:Body]
   message = params[:Body].split(' ')
   if message.length == 3
@@ -23,6 +29,7 @@ get '/receive' do
 
     gmail = Gmail.new(username, password)
     if command.eql? 'count'
+      puts gmail.inbox.count(:unread)
       @client.messages.create(
         from: '+19284332264',
         to: params[:From],
@@ -30,7 +37,12 @@ get '/receive' do
       )
     elsif command.eql? 'latest'
       gmail.peek = true
-      gmail.inbox.emails(:unread, :after => Date.today.prev_day)
+      puts gmail.inbox.emails(:unread, :after => Date.today.prev_day)[0].body
+      @client.messages.create(
+        from: '+19284332264',
+        to: params[:From],
+        body: "Hi Canaan, I know it's Friday, so I'll try to keep it short. In the past few weeks we were working on a new update, which is now a vailable on the AppStore. This version has ma ny improvements including a faster way to add tasks and notes right from the list, enhanced design, faster performance and support for iPhone 6 and 6 Plus. If you are enjoying 24me, it would be great if you could please leave a review. And as always I'd be happy for any feedback."
+      )
     end
   else
     @client.messages.create(
